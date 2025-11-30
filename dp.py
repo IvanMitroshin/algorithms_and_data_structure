a631db958d103681f8ce3cdff21276b561033a5e
Ivan Mitroshin <ivan.mtn39@gmail.com>
2025-11-30 15:09:13 +0200

    Динамическое программирование

---
+++ dp.py
@@ -0,0 +1,30 @@
+def knapsack(W, wt, val, n):
+    dp = [[0 for x in range(W + 1)] for x in range(n + 1)]
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 and w == 0:
+                dp[i][w] = 0
+            elif wt [i - 1] <= w:
+                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w])
+            else:
+                dp[i][w] = dp[i - 1][w]
+    return dp[n][W]
+
+def lcs(str1, str2):
+    m, n = len(str1), len(str2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[m][n]
+
+def count_n(n):
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    for num in range(1, n + 1):
+        for i in range(num, n + 1):
+            dp[i] += dp[i - num]
+    return dp[n]
